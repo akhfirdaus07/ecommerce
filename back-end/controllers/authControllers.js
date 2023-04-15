be276422349abb7269fa20bf0eaa1e2c1ffa46eb
akhfirdaus07 <121778146+akhfirdaus07@users.noreply.github.com>
2023-04-15 20:43:52 +0700

    Add login controllers

--- back-end/controllers/authControllers.js
+++ back-end/controllers/authControllers.js
@@ -1,14 +1,14 @@
 const bcrypt = require("bcrypt");
-const db=require("../models");
+const jwt = require("jsonwebtoken");
+const db = require("../models");
 const user = db.User;
 
 module.exports = {
   register: async (req, res) => {
     try {
-      const errorMessage = "Lengkapi Data";
       const { username, email, phone, storeName, password } = req.body;
       if (!username || !email || !phone || !storeName || !password)
-        throw errorMessage;
+        throw "Data is Incomplete";
 
       const salt = await bcrypt.genSalt(10);
       const hashPass = await bcrypt.hash(password, salt);
@@ -24,6 +24,43 @@ module.exports = {
         status: true,
         data: result,
         message: "Register Success",
+      });
+    } catch (err) {
+      res.status(400).send(err);
+    }
+  },
+  login: async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      if (!email || !password) throw "Invalid Email and/or Password";
+
+      const userExist = await user.findOne({
+        where: {
+          email,
+        },
+      });
+      if (!userExist)
+        throw {
+          status: false,
+          message: "User Not Found",
+        };
+
+      const isValid = await bcrypt.compare(password, userExist.password);
+      if (!isValid)
+        throw {
+          status: false,
+          message: "Wrong Password",
+        };
+
+      const payload = { id: userExist.id };
+      const token = jwt.sign(payload, "JWT", {
+        expiresIn: "1h",
+      });
+      res.status(200).send({
+        status: true,
+        message: "Login Success",
+        data: userExist,
+        token,
       });
     } catch (err) {
       res.status(400).send(err);
