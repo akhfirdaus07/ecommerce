ca7ec59848e8097b63ecc5f6054ca9251c22ffbe
akhfirdaus07 <121778146+akhfirdaus07@users.noreply.github.com>
2023-05-01 21:40:03 +0700

    Create controller to add income per day

--- back-end/controllers/reportControllers.js
+++ back-end/controllers/reportControllers.js
@@ -16,24 +16,52 @@ module.exports = {
       });
 
       //   Date filter, default to 7 days ago
-      const startDate = new Date(new Date().setDate(new Date().getDate() - 7)); //default to 7 days ago;
-      const endDate = new Date();
+      //   const startDate = new Date(new Date().setDate(new Date().getDate() - 7)); //default to 7 days ago;
 
-      //   const {startDate, endDate}=req.body;
+      //   const startDate= "2023-01-01"
+      //   const endDate = new Date();
+
+      const { startDate, endDate } = req.body;
 
       const transactionData = await transaction.findAll({
         where: {
           sellerId: userData.id,
           createdAt: {
+            // [Op.between]: [startDate, endDate]
             [Op.gte]: startDate,
             [Op.lte]: endDate,
           },
         },
       });
 
+      const getReport = (start, end) => {
+        let totalIncome;
+        for (
+          var arr = [], dt = new Date(start);
+          dt <= new Date(end);
+          dt.setDate(dt.getDate() + 1)
+        ) {
+          totalIncome = 0;
+          transactionData.forEach((transaction) => {
+            if (
+              dt.toDateString() ==
+              new Date(transaction.createdAt).toDateString()
+            ) {
+              totalIncome += transaction.totalAmount;
+            }
+          });
+          arr.push({ date: new Date(dt), totalIncome });
+        }
+        return arr;
+      };
+
+      const incomeByDay = getReport(new Date(startDate), new Date(endDate));
+      //   incomeByDay.map((v) => v.toISOString().slice(0, 10)).join("");
+
       res.status(200).send({
         status: true,
         userData,
+        incomeByDay,
         transactionData,
       });
     } catch (err) {
