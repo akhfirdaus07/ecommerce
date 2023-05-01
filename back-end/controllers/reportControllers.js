86455c6fbbb4cef8111f64110be74ca31e1a9b68
akhfirdaus07 <121778146+akhfirdaus07@users.noreply.github.com>
2023-05-01 20:19:39 +0700

    Add filter for date

--- back-end/controllers/reportControllers.js
+++ back-end/controllers/reportControllers.js
@@ -1,6 +1,7 @@
 const db = require("../models");
 const transaction = db.Transaction;
 const user = db.User;
+const { Op } = require("sequelize");
 
 module.exports = {
   income: async (req, res) => {
@@ -14,9 +15,19 @@ module.exports = {
         },
       });
 
+      //   Date filter, default to 7 days ago
+      const startDate = new Date(new Date().setDate(new Date().getDate() - 7)); //default to 7 days ago;
+      const endDate = new Date();
+
+      //   const {startDate, endDate}=req.body;
+
       const transactionData = await transaction.findAll({
         where: {
           sellerId: userData.id,
+          createdAt: {
+            [Op.gte]: startDate,
+            [Op.lte]: endDate,
+          },
         },
       });
 
